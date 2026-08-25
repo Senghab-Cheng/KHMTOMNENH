f3ff7f4973c9e18192dd9ecbc9fdd3c9b4d5b928
senghabcheng <scheng@paragoniu.edu.kh>
2026-08-25 14:29:26 +0700

    first commit

---
+++ src/main/java/com/fintrack/khmtomnenh/KhmtomnenhApplication.java
@@ -0,0 +1,13 @@
+package com.fintrack.khmtomnenh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhmtomnenhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhmtomnenhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fintrack/khmtomnenh/KhmtomnenhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintrack.khmtomnenh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhmtomnenhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
